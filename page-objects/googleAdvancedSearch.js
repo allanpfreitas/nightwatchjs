8c3712adbde95564d8058dbdb1acf0fdca28d56e
Allan Freitas <allanpetersoninfo@gmail.com>
2020-10-01 11:21:57 -0300

    Page Objects

--- page-objects/googleAdvancedSearch.js
+++ page-objects/googleAdvancedSearch.js
@@ -4,13 +4,28 @@ module.exports = {
         
         mainQueryInput : 'input[name="as_q"]',
         language : '#lr_button',
-        languageValue : '.goog-menuitem[value="lang_it"]',
+        //languageValue : '.goog-menuitem[value="lang_it"]',
         lastUpdateDropdown : '#as_qdr_button',
-        lastUpdateDropdownValue : '.goog-menuitem[value="m"]',
+        //lastUpdateDropdownValue : '.goog-menuitem[value="m"]',
         submitButton : '.jfk-button[type="submit"]',
     },
     // to add custom commands
     commands: [{
+        selectFilter(selector, value) {
+            return this
+                .click(selector)
+                .click(`.goog-menuitem[value="${value}"]`);
+        },
+
+        setQuery(value) {
+            return this
+                .setValue('@mainQueryInput', value)
+        },
+
+        search() {
+            return this
+                .click('@submitButton')
+        }
 
     }]
 }
--- tests/google.js
+++ tests/google.js
@@ -2,30 +2,30 @@ module.exports = {
     '@tags': ['google'],
     'Google Advanced Search: Elon Musk'(browser) {
         const mainQuery = "Elon Musk";
-        const mainQueryInputSelector = 'input[name="as_q"]';
-        const languageSelector = '#lr_button';
-        const languageValue = '.goog-menuitem[value="lang_it"]';
-        const lastUpdateDropdown = '#as_qdr_button';
-        const lastUpdateDropdownValue = '.goog-menuitem[value="m"]';
-        const submitButton = '.jfk-button[type="submit"]';
+
+        const page = browser.page.googleAdvancedSearch(); // declaring the page, it will expose all the elements and custom commands
+
         const resultPageQuerySelector = `#searchform input[name="q"][value="${mainQuery}"]`;
-        const languageResultPage = '[aria-label="Search Italian pages"]';
-        const timePeriodoResultPage = '[aria-label="Past month"]';
+        const languageResultPage = '//div[normalize-space(text())="Pesquisar páginas em Italiano"]';
+        const timePeriodoResultPage = '//div[normalize-space(text())="No último mês"]';
         
+        page
+            .navigate() //if i dont pass the url it will use the url defined in the page
+            .setQuery(mainQuery)
+            .selectFilter('@language', "lang_it")
+            .selectFilter('@lastUpdateDropdown', "m")
+            .search()
+
+            //.perform(() => { debugger; })
+
         browser
-            .url('https://www.google.com/advanced_search')
-            .setValue(mainQueryInputSelector, mainQuery)
-            .click(languageSelector)
-            .click(languageValue)
-            .click(lastUpdateDropdown)
-            .click(lastUpdateDropdownValue)
-            .click(submitButton)
             .assert.urlContains('as_q=Elon+Musk', 'Params: Query is Elon Musk')
             .assert.urlContains('lr=lang_it', 'Params: Language is Italian')
             .assert.urlContains('as_qdr=m', 'Params: Time period is Last Month')
             .assert.visible(resultPageQuerySelector, 'UI: Elon Musk is set in the search query')
-            .assert.containsText(languageResultPage,'Search Italian pages', 'UI: Is displaying the language selected')
-            .assert.containsText(timePeriodoResultPage, 'Past month', 'UI: Displays the time period selected')
+            .useXpath()
+            .assert.containsText(languageResultPage,'Pesquisar páginas em Italiano', 'UI: Is displaying the language selected')
+            .assert.containsText(timePeriodoResultPage, 'No último mês', 'UI: Displays the time period selected')
             .saveScreenshot('tests_output/google.png')
 
 
